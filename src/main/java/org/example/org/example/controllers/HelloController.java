b52305f97fa3bbf1b1c47637cc1961a3e9d44ec1
Jessica Zager <jmzager@gmail.com>
2021-12-13 19:46:33 -0600

    Finished Setup

---
+++ src/main/java/org/example/org/example/controllers/HelloController.java
@@ -0,0 +1,18 @@
+package org.example.org.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/")
+    public String sayHello(Model model) {
+        String name = System.getenv("VISITOR_NAME");
+        if (name == null) {
+            name = "World";
+        }
+        model.addAttribute("name", name);
+        return "index";
+    }
+}
