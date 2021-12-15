1a7e7b38a559b13f25f6f657d4c169c5e30a5585
Jessica Zager <jmzager@gmail.com>
2021-12-15 12:37:18 -0600

    Updated Controller & Index

--- src/main/java/org/example/org/example/controllers/HelloController.java
+++ src/main/java/org/example/org/example/controllers/HelloController.java
@@ -6,6 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
+
+    /**Notes from Tom's Repository lync0143
+     * Greet visitors on the main page.
+     *
+     * @param model model for the view
+     * @return name of the template file to use for the response
+     */
+
     @GetMapping("/")
     public String sayHello(Model model) {
         String name = System.getenv("VISITOR_NAME");
