96a121fe23158f9949dc07fbd43f47f5946d7558
Jessica Zager <jmzager@gmail.com>
2021-12-27 20:19:21 -0600

    Completed 1 Each, Passing & Failing Tests, at 34:00 into video

--- src/test/java/org/example/controllers/HelloControllerTest.java
+++ src/test/java/org/example/controllers/HelloControllerTest.java
@@ -4,15 +4,23 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HelloControllerTest {
+    @LocalServerPort
+    private int port;
+
     @Autowired
     private TestRestTemplate testRestTemplate;
 
 
     @Test()
     void testHelloMessage(){
-        assertThat(testRestTemplate.getForObject("http://localhost:8080/")).contains("Hello");
+        assertThat(testRestTemplate.getForObject("http://localhost:" + port, String.class)).contains("Hello");
+//        assertThat(testRestTemplate.getForObject("http://localhost:" + port, String.class)).contains("Goodbye");
+
     }
 }
